7c895e2923682b11ca43cc54619da3525822c08d
Putu Putra Eka Persada <83483873+PutuPutra@users.noreply.github.com>
2021-05-02 19:06:58 +0700

    Add files via upload

---
+++ Bagian Reverse.cpp
@@ -0,0 +1,10 @@
+char *reverse(char *carikata){
+    static char carikarakter[cols];
+    int bagian2 = 0;
+    for (int i = (strlen(carikata)-1); i >= 0; i--){
+        carikarakter[bagian2] = *(carikata+i);
+        bagian2++;
+    }
+
+    return carikarakter;
+}
